6f03c0f1ee6e864312b21f34c2df0ee1fe581013
Dan <dlaubacher3@gmail.com>
2017-10-10 20:26:10 -0400

    added basic login form

--- index.php
+++ index.php
@@ -11,6 +11,16 @@
   <title>Document</title>
 </head>
 <body>
+
   <h1> Login Logger Login </h1>
+  
+  <form class="" action="/app/login.php" method="post">
+    <label for="username">Name:</label>
+    <input type="text" name="username" value=""><br/>
+    <label for="password">Password:</label>
+    <input type="password" name="password" value=""><br/>
+    <button type="submit" name="login">Login</button>
+  </form>
+
 </body>
 </html>
